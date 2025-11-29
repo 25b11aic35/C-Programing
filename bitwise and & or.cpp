d11ec7b0cc5a431650045e93aa5c60d5a8a429c9
vaishnavi <vaishnaviraju183@gmail.com>
2025-11-29 10:38:07 +0530

    Add files via upload

---
+++ bitwise and & or.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,a,b;
+	printf("enter x,y values");
+	scanf("%d%d",a);
+	a=x&y;
+	printf("bitwise and=%d",a);
+	b=x|y;
+	printf("/nbitwise or=%d",b);
+	return 0;
+}
