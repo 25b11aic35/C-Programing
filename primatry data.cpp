723cf24b38e7e8a465553b917435897c09828b7c
vaishnavi <vaishnaviraju183@gmail.com>
2025-11-29 11:18:03 +0530

    Add files via upload

---
+++ primatry data.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x=10;
+	char ch='A';
+	float pi=3.14;
+	double p=12345.0679;
+	long int q=6553523;
+	printf("/nx=%d",x);
+	printf("/nch=%c",ch);
+	printf("/npi=%f",pi);
+	printf("/np=%lf",p);
+	printf("/nq=%ld",q);
+	return 0;
+}
